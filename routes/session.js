489ede91cd6bccbfd19413c1b8eb9a7b3b1da1fa
Elana Kopelevich <ekopelevich@gmail.com>
2016-10-03 18:28:07 -0600

    everything

--- routes/session.js
+++ routes/session.js
@@ -1,10 +1,65 @@
 'use strict';
 
+const bcrypt = require('bcrypt-as-promised');
+const boom = require('boom');
 const express = require('express');
+const knex = require('../knex');
+const { camelizeKeys } = require('humps');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-// YOUR CODE HERE
+router.get('/session', (req, res) => {
+  if (req.session.userId) {
+    return res.send(true);
+  }
+
+  res.send(false);
+});
+
+router.post('/session', (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !email.trim()) {
+    return next(boom.create(400, 'Email must not be blank'));
+  }
+
+  if (!password || password.length < 8) {
+    return next(boom.create(400, 'Password must not be blank'));
+  }
+
+  let user;
+
+  knex('users')
+    .where('email', email)
+    .first()
+    .then((row) => {
+      if (!row) {
+        throw boom.create(400, 'Bad email or password');
+      }
+
+      user = camelizeKeys(row);
+
+      return bcrypt.compare(password, user.hashedPassword);
+    })
+    .then(() => {
+      delete user.hashedPassword;
+
+      req.session.userId = user.id;
+
+      res.send(user);
+    })
+    .catch(bcrypt.MISMATCH_ERROR, () => {
+      throw boom.create(400, 'Bad email or password');
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete('/session', (req, res) => {
+  req.session = null;
+  res.send(true);
+});
 
 module.exports = router;
